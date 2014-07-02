28c71bb2866df72b869babdaf0e5bf18663e372a
zhangdi1 <melodyjiangcool@126.com>
2014-07-02 19:22:06 +0800

    jcrop control

--- app/scripts/views/home/cutpic-view.js
+++ app/scripts/views/home/cutpic-view.js
@@ -5,14 +5,14 @@ define(['talent'
 	,jst
 	,Jcrop
 ) {
-	return Talent.Layout.extend({
+	return Talent.ItemView.extend({
 
-		template: jst['home/index-page']
+		template: jst['home/cutpic-page']
 		,initialize: function() {
 
 		}
 		,ui:{
-			 "jcropSubmit" : ".jcrop_submit"
+			 "jcropSubmit" : ".jcrop_wrap a.crop_submit"
 		}
 		,events:function(){
 			var events = {};
@@ -21,45 +21,42 @@ define(['talent'
 		}
 		,raito:function(){
 			var self = this;
-			// setTimeout(function(){
-				self.raitoNo = 1;
-				var realwidth = parseInt($(".jcrop-holder img").css("width"));
-				var realheight = parseInt($(".jcrop-holder img").css("height"));
-				var cutwidth,cutheight;
-				if(realwidth > 500){
-					self.raitoNo = realwidth/500;
-				}
+			
+			this.raitoNo = 1;
+			var realwidth = parseInt(this.$el.find(".jcrop-holder img").css("width"));
+			var realheight = parseInt(this.$el.find(".jcrop-holder img").css("height"));
+			var cutwidth,cutheight;
+			if(realwidth > 500){
+				self.raitoNo = realwidth/500;
+			}
 
-				// 超过500缩小
-	            if(realwidth>500)   
-	            {   
-	                cutwidth=500;   
-	                cutheight=(realheight*500)/realwidth;   
-	            }else{
-	            	cutwidth = realwidth;
-	            	cutheight = realheight;
-	            }		
+			// 超过500缩小
+            if(realwidth>500)   
+            {   
+                cutwidth=500;   
+                cutheight=(realheight*500)/realwidth;   
+            }else{
+            	cutwidth = realwidth;
+            	cutheight = realheight;
+            }		
 
-		        $(".xuwanting").css({ "width" : cutwidth, "height" : cutheight });
-		        $(".jcrop-holder").css({ "width" : cutwidth, "height" : cutheight });
-		        $(".jcrop-tracker").css({ "width" : cutwidth, "height" : cutheight });
-		        $(".jcrop-holder img").css({ "width" : cutwidth, "height" : cutheight });
-			// },1000);
+	        this.$el.find(".xuwanting").css({ "width" : cutwidth, "height" : cutheight });
+	        this.$el.find(".jcrop-holder").css({ "width" : cutwidth, "height" : cutheight });
+	        this.$el.find(".jcrop-tracker").css({ "width" : cutwidth, "height" : cutheight });
+	        this.$el.find(".jcrop-holder img").css({ "width" : cutwidth, "height" : cutheight });
+			
 		}
 		,onRender: function() {
 		}
 		,onShow: function() {
 			var self = this;
 			var jcrop_api;
-			if(this.picObj!=undefined){
-				$(".xuwanting").attr("src",this.picObj.DownloadUrl);
-				$(".img_crop_preview").attr("src",this.picObj.DownloadUrl);
-			}
+			
 			$('body').on('jcrop:load', function(){
-				self.raito();	
+				self.raito();
 			});
 			//剪切头像
-			$(".xuwanting").Jcrop({
+			this.$el.find(".xuwanting").Jcrop({
 				aspectRatio:1,
 				onChange:showCoords,
 				onSelect:showCoords
@@ -68,23 +65,22 @@ define(['talent'
 				// jcrop_api.animateTo([100,100,400,300]);
 			});	
 
-
-			
 			//简单的事件处理程序，响应自onChange,onSelect事件，按照上面的Jcrop调用
 			function showCoords(obj){
 				 
-				$(".x").val(obj.x);
-				$(".y").val(obj.y);
-				$(".w").val(obj.w);
-				$(".h").val(obj.h);
+				self.$el.find(".x").val(obj.x);
+				self.$el.find(".y").val(obj.y);
+				self.$el.find(".w").val(obj.w);
+				self.$el.find(".h").val(obj.h);
+
 				if(parseInt(obj.w) > 0){
 					//计算预览区域图片缩放的比例，通过计算显示区域的宽度(与高度)与剪裁的宽度(与高度)之比得到
-					var rx = $(".preview_box").width() / obj.w; 
-					var ry = $(".preview_box").height() / obj.h;
+					var rx = self.$el.find(".preview_box").width() / obj.w; 
+					var ry = self.$el.find(".preview_box").height() / obj.h;
 					//通过比例值控制图片的样式与显示
-					$(".img_crop_preview").css({
-						width:Math.round(rx * $(".xuwanting").width()) + "px",	//预览图片宽度为计算比例值与原图片宽度的乘积
-						height:Math.round(rx * $(".xuwanting").height()) + "px",	//预览图片高度为计算比例值与原图片高度的乘积
+					self.$el.find(".img_crop_preview").css({
+						width:Math.round(rx * self.$el.find(".xuwanting").width()) + "px",	//预览图片宽度为计算比例值与原图片宽度的乘积
+						height:Math.round(rx * self.$el.find(".xuwanting").height()) + "px",	//预览图片高度为计算比例值与原图片高度的乘积
 						marginLeft:"-" + Math.round(rx * obj.x) + "px",
 						marginTop:"-" + Math.round(ry * obj.y) + "px"
 					});
@@ -95,12 +91,12 @@ define(['talent'
 		,onClose:function(){
 		}
 		,cropSubmit:function(){
-			if(parseInt($(".x").val())){
-				// $(".crop_form").submit();	
-				this.$el.find(".jcrop_x").html(parseInt($(".x").val()));
-				this.$el.find(".jcrop_y").html(parseInt($(".y").val()));
-				this.$el.find(".jcrop_w").html(parseInt($(".w").val()));
-				this.$el.find(".jcrop_h").html(parseInt($(".h").val()));
+			if(parseInt(this.$el.find(".x").val())){
+				
+				console.log("X坐标："+ parseInt(this.$el.find(".x").val()));
+				console.log("Y坐标："+ parseInt(this.$el.find(".y").val()));
+				console.log("宽："+ parseInt(this.$el.find(".w").val()));
+				console.log("高："+ parseInt(this.$el.find(".h").val()));
 			}else{
 				alert("要先在图片上划一个选区再单击确认剪裁的按钮哦！");	
 			}
--- app/scripts/views/home/index-page-view.js
+++ app/scripts/views/home/index-page-view.js
@@ -5,11 +5,21 @@ define(['talent'
 	,jst
 	,CutpicView
 ) {
+	var jst = (function(){
+	
+		this["JST"] = this["JST"] || {};
+	
+		this["JST"]["home/cutpic-page"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '\r\n<div class="zxx_main_con">\r\n    <div class="zxx_test_list">\r\n        <div class="rel mb20">\r\n            <img class="xuwanting" src="http://test.com/images/zd.jpg"  style="" />\r\n            <span class="preview_box crop_preview">\r\n                <img class="img_crop_preview" src="http://test.com/images/zd.jpg" />\r\n            </span>\r\n        </div>  \r\n        <form class="crop_form">\r\n            <input type="hidden" class="x" name="x" />\r\n            <input type="hidden" class="y" name="y" />\r\n            <input type="hidden" class="w" name="w" />\r\n            <input type="hidden" class="h" name="h" />\r\n            <a class="crop_submit save_btn"><span>确认剪裁</span></a>\r\n        </form>\r\n    </div>\r\n    <p>X坐标：<span class="jcrop_x"></span></p>\r\n    <p>Y坐标：<span class="jcrop_y"></span></p>\r\n    <p>宽：<span class="jcrop_w"></span></p>\r\n    <p>高：<span class="jcrop_h"></span></p>\r\n</div>\r\n';}return __p};
+
+  		this["JST"]["home/index-page"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<div class="cut_pic_wrap"></div>';}return __p};
+
+		return this["JST"];
+	})();
+	
 	var MainView = Talent.Layout.extend({
 
 		template: jst['home/index-page']
 		,initialize: function() {
-			this.cutpicView = new CutpicView();
 		}
 		,regions:{
 			"cutPic" : ".cut_pic_wrap"
@@ -24,6 +34,7 @@ define(['talent'
 		,onRender: function() {
 		}
 		,onShow: function() {
+			this.cutpicView = new CutpicView();
 			this.cutPic.show(this.cutpicView);
 		}
 		,onClose:function(){
